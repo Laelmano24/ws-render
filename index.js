9f5f872042a94a054faca5c74aef53b3bd554e93
Rael_Dev <162019823+Laelmano24@users.noreply.github.com>
2025-07-05 09:27:20 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const http = require('http')
+const { Server } = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: '*'
+  }
+})
+
+app.get('/', (req, res) => {
+  res.send('Servidor Socket.IO está rodando!')
+})
+
+io.on('connection', socket => {
+  console.log('Novo cliente conectado:', socket.id)
+
+  socket.emit('mensagem', 'Bem-vindo, cliente!')
+
+  socket.on('mensagem', msg => {
+    console.log('Mensagem do cliente:', msg)
+    io.emit('mensagem', msg)
+  })
+
+  socket.on('disconnect', () => {
+    console.log('Cliente desconectado:', socket.id)
+  })
+})
+
+const PORT = process.env.PORT || 3000
+server.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`)
+})
